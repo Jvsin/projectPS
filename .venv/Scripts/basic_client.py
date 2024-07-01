661818c15f62745eb0ba4e3f5cea87f8e0cda6e8
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-07-01 20:54:57 +0200

    basic client test file

--- .venv/Scripts/basic_client.py
+++ .venv/Scripts/basic_client.py
@@ -1,22 +1,17 @@
 import socket
 
 def main():
-    # Konfiguracja klienta
     host = '127.0.0.1'
     port = 12345
 
-    # Tworzenie gniazda klienta
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        # Łączenie z serwerem
         client_socket.connect((host, port))
         print(f'Połączono z serwerem {host}:{port}')
 
-        # Wysłanie wiadomości
         message = "Hello, Server!"
         client_socket.sendall(message.encode())
 
-        # Odbieranie odpowiedzi
         response = client_socket.recv(1024)
         print(f'Odpowiedź z serwera: {response.decode()}')
 
